91f68966e4f4329ecb11fda5050cdb95ca6b7c6a
João Pedro VL <64650215+JoaoPedroVL@users.noreply.github.com>
2024-06-15 14:04:12 -0300

    Add files via upload

---
+++ EXECICIO_RESOLVIDO/EXECICIO_RESOLVIDO/Entitdades/FuncionarioTerceirizado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXECICIO_RESOLVIDO.Entitdades
+{
+    internal class FuncionarioTerceirizado : Funcionario
+    {
+        public double DespesasAdicionais { get; set; }
+
+        public FuncionarioTerceirizado() { }
+
+        public FuncionarioTerceirizado(string nome, double valorHoraTrabalhada, int horas ,double despesasAdicionais)
+            : base(nome, valorHoraTrabalhada, horas)
+        {
+            DespesasAdicionais = despesasAdicionais;
+        }
+
+        public override double Pagamento()
+        {
+            return base.Pagamento() + 1.1 * DespesasAdicionais;
+
+        }
+    }
+}
